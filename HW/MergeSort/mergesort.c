e0ca2ce50eb1b00cb0cec06aeefa3c32348ecd63
Anura2425 <gmcclurecoleman@gmail.com>
2026-09-16 16:25:44 -0700

    progress on print and length

--- HW/MergeSort/mergesort.c
+++ HW/MergeSort/mergesort.c
@@ -17,3 +17,37 @@ int main(){
 	printf("Hello, World!\n");
 	return 0;
 }
+
+
+void printlist(struct node *head){
+	if(head == NULL){
+		printf("Empty List.");
+	}
+	struct node *curr = head;
+	while(curr->next != NULL){
+		printf("[%d]", curr->data);
+		curr = curr->next;
+	}
+}
+int getlistlength(struct node *head){
+	if(head == NULL){
+		return 0;
+	}
+	if(head->next == NULL){
+		return 1;
+	}
+	int length = 0;
+	struct node *curr = head;
+	while(curr->next != NULL){
+
+		curr = curr->next; //traverse
+	}
+
+
+}
+struct node* merge_sort(struct node* head, int size){
+
+}
+struct node* merge(struct node * head1, struct node * head2){
+
+}
